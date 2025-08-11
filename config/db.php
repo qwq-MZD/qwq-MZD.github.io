2050ece249c28d94249cb1f4ea62e4e621840ced
qwq-MZD <c20130227c@163.com>
2025-08-12 07:46:36 +0800

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'host' => 'localhost',
+    'dbname' => 'your_database_name',
+    'username' => 'your_db_username',
+    'password' => 'your_db_password',
+    'charset' => 'utf8mb4'
+];
